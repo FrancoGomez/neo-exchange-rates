ae0d4f3d59050b628047e447ec28dd16920e6f4a
FrancoGomez <mgomez21699@gmail.com>
2022-08-23 18:32:11 -0300

    Gets all the currencies and adds them to each dropdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-const obtenerTiposDeCambio = async (base) => {
-    let tiposDeCambio = await fetch(`https://api.exchangerate.host/lastest?base=${base}`)
-    let tiposDeCambioJSON = tiposDeCambio.json()
+const amountCurrency = document.querySelector("#amount-currency");
+const $fromCurrency = document.querySelector("#from-currency");
+const $toCurrency = document.querySelector("#to-currency");
 
-    return tiposDeCambioJSON
-}
+const getExchangeRates = async (base) => {
+    const exchangeRates = await fetch(
+        `https://api.exchangerate.host/lastest?base=${base}`
+    );
+    const exchangeRatesJSON = exchangeRates.json();
 
-obtenerTiposDeCambio("USD")
-    .then(respuesta => {console.log(respuesta.rates)})
+    return exchangeRatesJSON;
+};
+
+const createCurrencyDropdown = (currencies, $dropdown) => {
+    const $dropdownButton = $dropdown.children[0];
+
+    const $list = document.createElement("ul");
+    $list.className = "dropdown-menu";
+
+    $list.onclick = (e) => {
+        changeButtonCurrency(e.target.id, $dropdownButton);
+    };
+
+    currencies.forEach((currency) => {
+        $list.innerHTML += `<li>
+        <button class="dropdown-item" type="button" id=${currency}>
+            <div class="currency-flag currency-flag-${currency.toLowerCase()}"></div>
+            ${currency}
+        </button>
+        </li>`;
+    });
+
+    $dropdown.appendChild($list);
+};
+
+getExchangeRates("ARS").then((response) => {
+    createCurrencyDropdown(Object.keys(response.rates), $fromCurrency);
+    createCurrencyDropdown(Object.keys(response.rates), $toCurrency);
+});
+
+const changeButtonCurrency = (currency, $boton) => {
+    $boton.innerHTML = `
+    <div class="currency-flag currency-flag-${currency.toLowerCase()}"></div>
+    ${currency}`;
+};
