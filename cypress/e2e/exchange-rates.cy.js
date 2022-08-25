be70950865e0c49b6220ca585cba8f1b78f21442
FrancoGomez <mgomez21699@gmail.com>
2022-08-24 22:31:29 -0300

    Simplify and refactor code

--- cypress/e2e/exchange-rates.cy.js
+++ cypress/e2e/exchange-rates.cy.js
@@ -1,13 +1,15 @@
 /// <reference types="Cypress" />;
 
 describe("Test exchange rates", () => {
-    const from = "EUR";
-    const to = "USD";
-    const amount = 2500;
+    const props = {
+        from: "EUR",
+        to: "USD",
+        amount: 2500,
+    };
 
     it("Test to see if it shows exchanges rates", () => {
         cy.visit("http://127.0.0.1:5500/index.html");
-        cy.get("#amount-currency input").type(`{backspace}${amount}`);
+        cy.get("#amount-currency input").type(`{backspace}${props.amount}`);
 
         cy.get("#from-currency.dropdown").click();
         cy.get("#fromEUR").click();
@@ -18,33 +20,32 @@ describe("Test exchange rates", () => {
         cy.get("#convert").click();
     });
 
-    it("Test to see if the showed exchange rates are correct", () => {
-        convertCurrencyCypress(from, to, amount).then(({ query, result }) => {
-            const styledResult = result.toFixed(6);
-            cy.get("#from-conversion").contains(
-                `$${query.amount.toFixed(2)} ${query.from} =`
-            );
-            cy.get("#to-conversion").contains(`$${styledResult} ${query.to}`);
-            cy.get("#reverse-conversion").contains(
-                `$${query.amount.toFixed(2)} ${
-                    query.to
-                } = $${getReverseExchangeRateCypress(styledResult)} ${
-                    query.from
-                }`
-            );
-        });
+    it("Test to see if the showed exchange rates are correct", async () => {
+        const { query, result } = await convertCurrencyCypress(props);
+
+        const styledResult = result.toFixed(6);
+
+        cy.get("#from-conversion").contains(
+            `$${query.amount.toFixed(2)} ${query.from} =`
+        );
+        cy.get("#to-conversion").contains(`$${styledResult} ${query.to}`);
+        cy.get("#reverse-conversion").contains(
+            `$${query.amount.toFixed(2)} ${
+                query.to
+            } = $${getReverseExchangeRateCypress(styledResult)} ${query.from}`
+        );
     });
 });
 
-const convertCurrencyCypress = async (from, to, amount) => {
+const convertCurrencyCypress = async ({ from, to, amount }) => {
     const conversion = await fetch(
         `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`
     );
-    const conversionJSON = conversion.json();
 
-    return conversionJSON;
+    return conversion.json();
 };
 
 const getReverseExchangeRateCypress = (rate) => {
-    return (1 / rate).toFixed(6);
+    const inverseRate = 1 / rate;
+    return inverseRate.toFixed(6);
 };
